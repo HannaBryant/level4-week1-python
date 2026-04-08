5f0a806a283959fde0e71050c10c0df56b8ad486
Hanna Bryant <hannab329@gmail.com>
2026-04-08 02:32:09 +0000

    first assignment complete

---
+++ enrollment.py
@@ -0,0 +1,15 @@
+first_name = "Hanna"
+last_name = 'Bryant'
+age = 26
+tuition_balance = 7843.98
+is_active = True
+
+print("-------Student Enrollment Report-------")
+print(f"First Name      : {first_name}")
+print(f"Last Name      : {last_name}")
+print(f"Age             : {age}")
+print(f"Tuition Balance  : {tuition_balance}")
+print(f"Enrollment Status : {'Active' if is_active else 'Inactive'}")
+print("---------------------------------------")
+
+del tuition_balance
