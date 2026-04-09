a6d04931f03928c8904a50f6c9ba58526bd7576e
Hanna Bryant <hannab329@gmail.com>
2026-04-09 01:21:33 +0000

    fourth assignment complete

---
+++ calculator.py
@@ -0,0 +1,8 @@
+base_tuition = 6900
+registration_fee = 100
+
+total = base_tuition + registration_fee
+
+discounted_price = total - 50
+
+print(f"The total tuition is {total} and the discounted price is {discounted_price}.")
